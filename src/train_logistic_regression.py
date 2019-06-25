3b0c187ba61debbc4e3ad53dbd37cdfa2ec97e06
alexandrusoloms <mariussolomes@gmail.com>
2019-06-25 19:40:08 +0100

    implemented a better  that actually generates data batches

--- src/train_logistic_regression.py
+++ src/train_logistic_regression.py
@@ -1,25 +1,18 @@
-import os
+"""
+this script trains a logistic regression on the ff1010bird and warblrb10k train set.
 
+`YieldItems` yields data, label pairs of a set size. It uses `yield`, returning a generator, making this
+memory efficient.
+"""
 import pickle
-import numpy as np
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 
 
-from .Spectrogram import Spectrogram
-from .train_model_utils import YieldItems
+from .train_model_utils import make_path, YieldItems
 
 
-def make_path(path_name):
-    return os.path.join(os.path.dirname("__file__"), "", ) + path_name
-
-
-def load_data_item(file_path):
-    spectrogram_data = Spectrogram(file_path=file_path).process()
-    shape = (len(spectrogram_data) // 40, 40)
-    spectrogram_data = np.array(spectrogram_data).reshape(shape)
-    return spectrogram_data
-
+MAX_SHAPE = 950
 
 data_paths = [
     make_path(path_name="/homes/amsolomes1/birdSongData/ff1010bird/wav/"),
@@ -33,39 +26,33 @@ master_bird_dataset = pd.concat([
 
 master_bird_dataset["itemid"] = master_bird_dataset["itemid"].apply(func=lambda x: str(x))
 
-path_yield = YieldItems.yield_from_path(data_paths=data_paths, batch_size=100)
-
-MAX_SHAPE = 950
-
-logistic_regression_clf = LogisticRegression()
+# `YieldItems` will implement the feature extraction process from a wav file
+#  it will create N batches of length `batch_size` containing:
+#           1. numerical representation of a spectrogram of shape (MAX_SHAPE+1, 1)
+#           2. a label
+#
+path_yield = YieldItems.yield_from_path(data_paths=data_paths, batch_size=100, master_bird_dataset=master_bird_dataset,
+                                        max_shape=MAX_SHAPE)
 
 test_batch = list()
 test_label = list()
+train_batch = list()
+train_label = list()
+
 index = 0
-
+logistic_regression_clf = LogisticRegression()
+# The first item yielded from `YieldItems` is the test batch and test label,
+# and so we need to take it into account
 for i in path_yield:
-    train_batch = list()
-    train_label = list()
     print("loading data...")
-    for (train_path, bird_id) in i:
-        spec = load_data_item(file_path=train_path)
-        spec = np.mean(spec, axis=1)[:MAX_SHAPE]
-        spec = np.append(1, spec)
-        if spec.shape[0] > MAX_SHAPE:
-
-            if index == 0:
-                # it is the test data
-                test_batch.append(spec)
-                test_label.append(master_bird_dataset[master_bird_dataset["itemid"] == bird_id]["hasbird"].values[0])
-            else:
-                train_batch.append(spec)
-                train_label.append(master_bird_dataset[master_bird_dataset["itemid"] == bird_id]["hasbird"].values[0])
-    if index > 0:
-        # Training
-        print("training model...", index)
+    if index != 0:
+        train_batch, train_label = i
         logistic_regression_clf.fit(train_batch, train_label)
+    else:
+        test_batch, test_label = i
     index += 1
 
+# saving model
 with open("logistic_regression_clf.pickle", "wb") as handle:
     pickle.dump(logistic_regression_clf, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
